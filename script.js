b8fffe9758b1a6482203f0067ecca11def2de504
pheranmy7 <pheranmyolabiyi@gmail.com>
2022-09-03 12:34:53 +0100

    portfolio commit

---
+++ script.js
@@ -0,0 +1,7 @@
+ /** typing animation */
+ var typed = new Typed(".typing",{
+     strings:["", "Fullstack Web Developer", "Graphic Designer", "Software Developer"],
+     typeSpeed:100,
+     BackSpeed:60,
+     loop:true
+ })
