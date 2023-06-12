5c6e0d2dc76848fad5ab48cac9c49daa0295139a
devF <pheranmyolabiyi@gmail.com>
2023-06-12 17:58:16 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
  /** typing animation */
  var typed = new Typed(".typing",{
-     strings:["", "Fullstack Web Developer", "Graphic Designer", "Software Developer"],
+     strings:["", "Backend Developer", "UI Designer", "Flutter Developer "],
      typeSpeed:100,
      BackSpeed:60,
      loop:true
